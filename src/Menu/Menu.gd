c3598be236f8a4e33065310b04c1efa0287c0ed2
smallcluster <small.cluster@gmail.com>
2021-11-28 10:11:41 +0400

    add intro and set main scene

--- src/Menu/Menu.gd
+++ src/Menu/Menu.gd
@@ -14,7 +14,7 @@ func _ready():
 
 
 func _on_Start_pressed():
-	get_tree().change_scene("")
+	get_tree().change_scene("res://niveaux/intro.tscn")
 
 func _on_Credits_pressed():
 	get_tree().change_scene("res://Credits/Credit.tscn")
